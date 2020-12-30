65967b56eba5ea137e1078e0dfde50013e2dbfe6
Afnando <afnanhasnuddin99@gmail.com>
2020-12-30 13:36:58 +0000

    client-order supply

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+import json
+def get_input(clientSocket):
+   no = input("Enter Item code::")
+   clientSocket.send(no.encode())
+
+clientSocket = socket.socket()
+host = "192.168.120.11"
+port = 8888
+
+try:
+  clientSocket.connect((host,port))
+
+except socket.error as e:
+  print(str(e))
+
+response = clientSocket.recv(1024).decode()
+d = json.loads(response)
+while True:
+   print("Skincare Supplier")
+   print("-----------------")
+   for keys in d.keys():
+         name , cost = d[keys]
+         print('Item Code ->',keys,'Product -> :',name,'\n','The cost is :',cost)
+   
+   option = input('\nYour option:')
+   clientSocket.send(str.encode(option))
+   product =clientSocket.recv(2048).decode()
+   print("Product select:",product)
+   result=clientSocket.recv(2048).decode()
+   print("Order Total:",result,)
+   print("*-------------------------------*\n")
+
+clientSocket.close()
