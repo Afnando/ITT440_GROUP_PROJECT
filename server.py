c227c5302818d7e5fe4546e71a0a07ae05b53e12
Afnando <afnanhasnuddin99@gmail.com>
2021-01-05 08:29:58 +0000

    write file to list item

--- server.py
+++ server.py
@@ -6,10 +6,14 @@ import errno
 from multiprocessing import Process
 import math
 import json
+import datetime
 
 d = {'1':('Cleanser',30.45),'2':('Exfoliator ',95.00),'3':('Serum',78.80),'4':('Sunscreen',47.50)}
 data = json.dumps(d)
 sum=0
+n=[]
+noProcess = 0
+
 def process_start(s_sock):
   global data,sum
   s_sock.send(str.encode(data))
@@ -18,11 +22,27 @@ def process_start(s_sock):
     opt = s_sock.recv(2048)
     name , cost = d[str(opt.decode('ascii'))]
     s_sock.send(name.encode())
+   # print(
     print('Product selected-> :',name,'\n','Cost-> :',cost)
     sum+=cost
     s_sock.send(bytes(str(sum),'ascii'))
+    n.append([name,cost])
+    receipt_file(n)
     
   s_sock.close()
+
+def receipt_file(n):
+
+  date_time = datetime.date.today()
+  date_time_str = str(date_time)
+  extension = ".txt"
+  filename = date_time_str + extension
+
+  with open(filename, 'w') as f:
+      for name in n:
+         f.write('%s\n' % name)
+  f.close()
+
 if __name__ == '__main__':
 
   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -37,6 +57,8 @@ if __name__ == '__main__':
         print ("\nConnection from : ", s_addr)
         p = Process(target = process_start,args = (s_sock,))
         p.start()
+        noProcess += 1
+        print("Client:" , str(noProcess))
 
       except socket.error:
         print('got a socket error')
