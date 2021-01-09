08ca83c0aa24b0af062dbe22d5ee6cb84d9b546a
Afnando <afnanhasnuddin99@gmail.com>
2021-01-09 10:23:30 +0000

    change write file func

--- server.py
+++ server.py
@@ -7,12 +7,13 @@ from multiprocessing import Process
 import math
 import json
 import datetime
+from tabulate import tabulate
 
 d = {'1':('Cleanser',30.45),'2':('Exfoliator ',95.00),'3':('Serum',78.80),'4':('Sunscreen',47.50)}
 data = json.dumps(d)
-sum=0
-n=[]
-noProcess = 0
+sum = 0
+n = []
+qty = 0
 
 def process_start(s_sock):
   global data,sum
@@ -20,18 +21,43 @@ def process_start(s_sock):
 
   while True:
     opt = s_sock.recv(2048)
-    name , cost = d[str(opt.decode('ascii'))]
-    s_sock.send(name.encode())
-   # print(
-    print('Product selected-> :',name,'\n','Cost-> :',cost)
-    sum+=cost
-    s_sock.send(bytes(str(sum),'ascii'))
-    n.append([name,cost])
-    receipt_file(n)
-    
+
+    if str(opt.decode('ascii')) in d.keys():
+
+        s_sock.send(b"YES")
+        name , cost = d[str(opt.decode('ascii'))]
+        qty = s_sock.recv(2048).decode('utf-8')
+        print(qty)        
+        s_sock.send(name.encode())
+        print('Product selected-> :',name,'\n','Cost-> :',cost)
+        sum=float(sum)+(float(qty)*float(cost))
+        print('Total->:',sum)
+        s_sock.send(bytes(str(sum),'ascii'))
+
+        #insert data to list
+        n.append([name,cost,qty,sum])
+
+    elif str(opt.decode('ascii')) == '99':
+      
+        s_sock.send(b"FINISH")
+        #insert list to table
+        x = (tabulate(n, headers=['Product Name', 'Price','Quantity','Amount(RM)'],tablefmt="grid",colalign=("center","center","center","center")))
+
+        #write table into file
+        receipt_file(x)
+       
+        # send_file()
+        print("Connection endend")
+        break
+
+    else:
+        s_sock.sendall(b"NO")
+        print('No matched item code')
+        continue
+
   s_sock.close()
 
-def receipt_file(n):
+def receipt_file(x):
 
   date_time = datetime.date.today()
   date_time_str = str(date_time)
@@ -39,8 +65,12 @@ def receipt_file(n):
   filename = date_time_str + extension
 
   with open(filename, 'w') as f:
-      for name in n:
-         f.write('%s\n' % name)
+      f.write("Order Information\n\n")
+      f.close()
+
+  with open(filename, 'a') as f:
+      f.write(x)
+
   f.close()
 
 if __name__ == '__main__':
@@ -57,8 +87,6 @@ if __name__ == '__main__':
         print ("\nConnection from : ", s_addr)
         p = Process(target = process_start,args = (s_sock,))
         p.start()
-        noProcess += 1
-        print("Client:" , str(noProcess))
 
       except socket.error:
         print('got a socket error')
