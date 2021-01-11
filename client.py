dee91d4cb1581c32cc901dc9c3fef34fe3b1fb3c
hafiz-svg <73226271+hafiz-svg@users.noreply.github.com>
2021-01-11 12:04:16 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import socket
 import json
+import sys
+import os
+import struct
 
 clientSocket = socket.socket()
 host = "192.168.120.11"
@@ -21,13 +24,34 @@ while True:
          print('Item Code ->',keys,'Product -> :',name,'\n','The cost is :',cost)
    
    option = input('\nYour option:')
-   clientSocket.send(str.encode(option))
-   quantity = input('Number quantity:')
-   clientSocket.send(str.encode(quantity))
-   product =clientSocket.recv(2048).decode()
-   print("Product select:",product)
-   result=clientSocket.recv(2048).decode()
-   print("Order Total:",result,)
-   print("*-------------------------------*\n")
+   clientSocket.send(str.encode(option.strip()))
+   check = clientSocket.recv(1024).decode()
+   if check == 'YES':
+      quantity = input('Number quantity:')
+      clientSocket.send(str.encode(quantity))
+      product =clientSocket.recv(2048).decode()
+      print("\n*-------------------------------*")
+      print("Product select:",product)
+      result=clientSocket.recv(2048).decode()   
+      amount,total = result.split('-')
+      print("Product sum:RM",amount)
+      print("Order Total:",result,)
+      print("*-------------------------------*\n")
+ elif check == 'FINISH':
+
+      #recieve receipt file
+      received = clientSocket.recv(1024).decode()
+      filename = received
+      with open(filename,"wb") as f:
+
+         data=clientSocket.recv(1024)
+         f.write(data)
+         f.close()
+
+      print('Session Ended')
+      exit(0)
+
+   elif check == 'NO':
+      print("No Matched Item Code!!!\n")
 
 clientSocket.close()
