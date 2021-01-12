0c05c974479cad73d04a50cde483b63eae3f8eff
Mahfudz <72877320+LemonTeaIce@users.noreply.github.com>
2021-01-13 00:44:03 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ from tabulate import tabulate
 
 d = {'1':('Cleanser',30.45),'2':('Exfoliator',95.00),'3':('Serum',78.80),'4':('Sunscreen',47.50)}
 data = json.dumps(d)
-
 m= 0
 sum = 0
 n = []
@@ -19,10 +18,6 @@ n1 = []
 qty =0
 sum2 = 0
 tott=0
-=======
-sum = 0
-n = []
-
 
 def process_start(s_sock):
   global data,sum,tott
@@ -34,15 +29,10 @@ def process_start(s_sock):
 
   n=[]
   while True:
-   # name = s_sock.recv(2048)
-   # s_sock.send(str.encode(name))
-   # num = s_sock.recv(2048)
-   # s_sock.send(str.encode(num))
     
     opt = s_sock.recv(2048)
 
     if str(opt.decode('ascii')) in d.keys():
-
     
       s_sock.send(b"YES")
       name,cost = d[str(opt.decode('ascii'))]
@@ -65,23 +55,8 @@ def process_start(s_sock):
       ans = s_sock.recv(2048).decode('utf-8')
       print ("ans")
       print (ans)
-      #sum2=0
-     # for list in range(len(n)):
-    #    for value in range(len(n[list])):
-   #       name = n[list][0]
-  #        cost = n[list][1]
- #         qty = n[list][2]
-#          sum = n[list][1]*int(n[list][2])
-       # sum2 = sum2 +sum
-      #  n1.append([name,cost,qty,sum])
-     #   print("n1")
-    #    print (n1)
-   #     print ("Sum:")
-  #      print (sum)
- #       print ("Sum2:")
-#        print (sum2)
 
-      if ans == "YES":
+      if ans == "YES":  #if client want to delete
         s_sock.send(bytes(str(ans),'ascii'))
         ans1 = s_sock.recv(2048).decode('utf-8')
         fb1 = [item[0] for item in n]
@@ -93,7 +68,7 @@ def process_start(s_sock):
           print ("fb1")
           print (fb1)
 
-          if ans2 == "YES":
+          if ans2 == "YES":# if client want to delete a list of product and start procee delete based on product name
 
             tot2 =0
             tot =0
@@ -118,7 +93,7 @@ def process_start(s_sock):
             tot1 = tot1+tot1
             global tott1
             tott=0
-            for i in range(len(n)):
+            for i in range(len(n)):  #total product after delete
               for j in range (len(n[i])):
                 print(n[i][3])
                 tott1= n[i][3]
@@ -135,7 +110,7 @@ def process_start(s_sock):
             s_sock.send(bytes(str(tott),'ascii'))
       
       else:
-        print("Client Dont delete anything..")
+        print("Client Dont delete anything..") # if client dont want to delete
         sum2=0
 
         for list in range(len(n)):
@@ -160,6 +135,7 @@ def process_start(s_sock):
         ans55 =s_sock.recv(2048).decode('utf-8')
         print (ans55)
         s_sock.send(bytes(str(n),'ascii'))
+        
       #insert list to table
       x = (tabulate(n, headers=['Product Name','Price','Quantity','Amount(RM)'],tablefmt="grid",colalign=("center","center","center","center")))
       y = name1
@@ -180,43 +156,6 @@ def process_start(s_sock):
       continue
 
 
-        s_sock.send(b"YES")
-        name , cost = d[str(opt.decode('ascii'))]
-        qty = s_sock.recv(2048).decode('utf-8')
-        print(qty)        
-        s_sock.send(name.encode())
-        print('Product selected-> :',name,'\n','Cost-> :',cost)
-        sum=float(sum)+(float(qty)*float(cost))
-        print('Total->:',sum)
-        s_sock.send(bytes(str(sum),'ascii'))
-
-        #insert data to list
-        n.append([name,cost,qty,sum])
-
-    elif str(opt.decode('ascii')) == '99':
-      
-        s_sock.send(b"FINISH")
-        #insert list to table
-        x = (tabulate(n, headers=['Product Name', 'Price','Quantity','Amount(RM)'],tablefmt="grid",colalign=("center","center","center","center")))
-
-        #write table into file
-        receipt_file(x)
-       
-        # send_file()
-        print("Connection endend")
-        break
-
-    else:
-        s_sock.sendall(b"NO")
-        print('No matched item code')
-        continue
-
-  s_sock.close()
-
-def receipt_file(x):
-
-
-
   s_sock.close()
 
 def receipt_file(x,y,z,z1):
@@ -224,7 +163,6 @@ def receipt_file(x,y,z,z1):
   date_time_str = str(date_time)
   extension = ".txt"
   filename = date_time_str + extension
-
 
   with open(filename,'w') as f:
     f.write("Name: ")
@@ -238,14 +176,6 @@ def receipt_file(x,y,z,z1):
 
   with open(filename, 'a') as f:
     f.write(x)
-
-  with open(filename, 'w') as f:
-      f.write("Order Information\n\n")
-      f.close()
-
-  with open(filename, 'a') as f:
-      f.write(x)
-
 
   f.close()
 
@@ -261,27 +191,6 @@ def send_file():
     s_sock.sendall(sendF)
     f.close()
 
-def delete_detail(n,y):
-  print ("y")
-  print (y)
-  print ("Nilai n")
-  print ("n[0]")
-  print (n[0])
-  print ("n[1]")
-  print (n[1])
-  print ("Delete n[y]")
-  print (n[y])
-  del n[y]
-  print ("N after")
-  print (n)
-  for i in range(len(n)):
-    tot1 =0
-    print("tot in func")
-    for j in range(len(n[i])):
-      tot= n[i][3]
-      print (n[i][j])
-    tot1= tot1+tot
-    print (tot1)
 
 if __name__ == '__main__':
 
